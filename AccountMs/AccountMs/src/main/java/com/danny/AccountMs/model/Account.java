b368383a5f5aeb81f7f01e16e025b54956267961
dverapal <dannysteep.verapalomino@.nttdata.com>
2024-10-02 18:06:33 -0500

    Create Account model

---
+++ AccountMs/AccountMs/src/main/java/com/danny/AccountMs/model/Account.java
@@ -0,0 +1,25 @@
+package com.danny.AccountMs.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String numeroCuenta ;
+    private Double saldo;
+    private TipoCuenta tipoCuenta;
+    private UUID clientId;
+}
