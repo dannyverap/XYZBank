0fb74b5dd263175c1b641848f3203408627fde1a
dverapal <dannysteep.verapalomino@.nttdata.com>
2024-10-02 18:04:04 -0500

    Create Account microservices

---
+++ AccountMs/AccountMs/src/test/java/com/danny/AccountMs/AccountMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danny.AccountMs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
