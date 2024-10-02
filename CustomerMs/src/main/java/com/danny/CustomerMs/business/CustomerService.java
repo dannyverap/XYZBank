4b9b06b4ad26e08a7635d7fdb43733a2b1d1a49c
dverapal <dannysteep.verapalomino@.nttdata.com>
2024-10-02 14:02:54 -0500

    Remove throws exceptions on CustomerService interface

--- CustomerMs/src/main/java/com/danny/CustomerMs/business/CustomerService.java
+++ CustomerMs/src/main/java/com/danny/CustomerMs/business/CustomerService.java
@@ -8,9 +8,9 @@ import java.util.List;
 import java.util.UUID;
 
 public interface CustomerService {
-    CustomerResponse createCustomer (CustomerRequest customer) throws Exception;;
-    List<CustomerResponse> getCustomers(int Limit, int offset) throws Exception;;
-    CustomerResponse getCustomerDetails(UUID id) throws Exception;;
-    CustomerResponse updateCustomer(UUID id, CustomerRequest newCustomerData) throws Exception;
-    ModelApiResponse deleteCustomer(UUID id) throws Exception;
+    CustomerResponse createCustomer (CustomerRequest customer);
+    List<CustomerResponse> getCustomers(int Limit, int offset);
+    CustomerResponse getCustomerDetails(UUID id);
+    CustomerResponse updateCustomer(UUID id, CustomerRequest newCustomerData);
+    ModelApiResponse deleteCustomer(UUID id);
 }
