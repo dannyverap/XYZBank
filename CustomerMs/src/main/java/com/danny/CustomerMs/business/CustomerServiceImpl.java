895fc9ab128eabbbf1fce628d49645a5c7e3283b
dverapal <dannysteep.verapalomino@.nttdata.com>
2024-10-02 14:17:03 -0500

    Improve service business logic and add customer errors

--- CustomerMs/src/main/java/com/danny/CustomerMs/business/CustomerServiceImpl.java
+++ CustomerMs/src/main/java/com/danny/CustomerMs/business/CustomerServiceImpl.java
@@ -1,11 +1,15 @@
 package com.danny.CustomerMs.business;
 
+import com.danny.CustomerMs.exception.BadPetitionException;
+import com.danny.CustomerMs.exception.ConflictException;
+import com.danny.CustomerMs.exception.NotFoundException;
 import com.danny.CustomerMs.model.Customer;
 import com.danny.CustomerMs.model.CustomerRequest;
 import com.danny.CustomerMs.model.CustomerResponse;
 import com.danny.CustomerMs.model.ModelApiResponse;
 import com.danny.CustomerMs.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -23,47 +27,53 @@ public class CustomerServiceImpl implements CustomerService {
     @Autowired
     CustomerMapper customerMapper;
 
-    private Boolean findIfCustomerExistsByEmail(String email) {
-        return this.customerRepository.existsByEmail(email);
-    }
-
     @Override
-    public CustomerResponse createCustomer(CustomerRequest customerRequest) throws Exception {
+    public CustomerResponse createCustomer(CustomerRequest customerRequest) {
         if (this.customerRepository.existsByEmail(customerRequest.getEmail())) {
-            throw new Exception("Email ya registrado en DB");
+            throw new ConflictException("Email ya registrado");
         }
         if (this.customerRepository.existsByDni(customerRequest.getDni())) {
-            throw new Exception("DNI ya registrado");
+            throw new ConflictException("DNI ya registrado");
         }
-
         Customer customer = this.customerMapper.getCustomerFromRequest(customerRequest);
         this.customerRepository.save(customer);
         return this.customerMapper.getCustomerResponseFromCustomer(customer);
     }
-
+//Todo validate integer limit and offset
     @Override
     public List<CustomerResponse> getCustomers(int limit, int offset) {
+        offset = (offset >= 0) ? offset: 0;
+        limit= ( limit > 0 ) ? limit: 20;
         Pageable pageable = PageRequest.of(offset, limit);
-        List<Customer> customers = this.customerRepository.findAll();
+        Page<Customer> customers = this.customerRepository.findAll(pageable);
         return customers.stream().map(this.customerMapper::getCustomerResponseFromCustomer).collect(Collectors.toUnmodifiableList());
     }
 
     @Override
     public CustomerResponse getCustomerDetails(UUID id) {
-        return this.customerMapper.getCustomerResponseFromCustomer(this.customerRepository.findById(id).get());
+        return this.customerMapper.getCustomerResponseFromCustomer(this.customerRepository.findById(id).orElseThrow(() -> new NotFoundException("Cliente no encontrado")));
     }
 
     @Override
-    public CustomerResponse updateCustomer(UUID id, CustomerRequest customerRequest) throws Exception {
-        Optional<Customer> customerInDB = this.customerRepository.findById(id);
+    public CustomerResponse updateCustomer(UUID id, CustomerRequest customerRequest) {
+        Optional<Customer> customerToUpdate = this.customerRepository.findById(id);
 
-        if (customerInDB.isEmpty()) throw new Exception("Not found");
-        if (this.findIfCustomerExistsByEmail(customerRequest.getEmail())) {
-            throw new Exception("Email ya registrado en DB");
+        if (customerToUpdate.isEmpty()) {
+            throw new NotFoundException("Not found");
         }
-        customerInDB.ifPresent(customer -> {
+
+        customerToUpdate.ifPresent(customer -> {
             if (!customer.getEmail().equals(customerRequest.getEmail())) {
+                if (this.customerRepository.existsByEmail(customerRequest.getEmail())) {
+                    throw new BadPetitionException("Email ya registrado en otro usuario");
+                }
                 customer.setEmail(customerRequest.getEmail());
+            }
+            if (!customer.getDni().equals(customerRequest.getDni())) {
+                if (this.customerRepository.existsByDni(customerRequest.getDni())) {
+                    throw new BadPetitionException("DNI ya registrado en otro usuario");
+                }
+                customer.setDni(customerRequest.getDni());
             }
             if (!customer.getNombre().equals(customerRequest.getNombre())) {
                 customer.setNombre(customerRequest.getNombre());
@@ -72,18 +82,17 @@ public class CustomerServiceImpl implements CustomerService {
                 customer.setApellido(customerRequest.getApellido());
             }
         });
-        Customer updatedCustomer = this.customerRepository.save(customerInDB.get());
+        Customer updatedCustomer = this.customerRepository.save(customerToUpdate.get());
         return this.customerMapper.getCustomerResponseFromCustomer(updatedCustomer);
     }
 
     @Override
     public ModelApiResponse deleteCustomer(UUID id) {
-        ModelApiResponse response = new ModelApiResponse();
         if (!this.customerRepository.existsById(id)) {
-            response.setMessage("Cliente no encontrado");
-            return response;
+            throw new NotFoundException("Cliente no existe y ya se encuentra eliminado");
         }
         this.customerRepository.deleteById(id);
+        ModelApiResponse response = new ModelApiResponse();
         response.setMessage("Cliente borrado exitosamente");
         return response;
     }
