cb704c2f994c5f7198ddc6e3f2e5328db86a8865
dverapal <dannysteep.verapalomino@.nttdata.com>
2024-10-02 14:03:58 -0500

    Remove trycatchs on CustomerDelegateImpl

--- CustomerMs/src/main/java/com/danny/CustomerMs/CustomerDelegateImpl.java
+++ CustomerMs/src/main/java/com/danny/CustomerMs/CustomerDelegateImpl.java
@@ -7,12 +7,12 @@ import com.danny.CustomerMs.model.CustomerResponse;
 import com.danny.CustomerMs.model.ModelApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.UUID;
 
-@Service
+@Component
 public class CustomerDelegateImpl implements CustomerApiDelegate {
 
     @Autowired
@@ -21,46 +21,26 @@ public class CustomerDelegateImpl implements CustomerApiDelegate {
 
     @Override
     public ResponseEntity<CustomerResponse> createCustomer(CustomerRequest customerRequest) {
-        try {
-            return ResponseEntity.ok(this.customerService.createCustomer(customerRequest));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        return ResponseEntity.ok(this.customerService.createCustomer(customerRequest));
     }
 
     @Override
     public ResponseEntity<ModelApiResponse> deleteCustomer(UUID id) {
-        try {
-            return ResponseEntity.ok(this.customerService.deleteCustomer(id));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        return ResponseEntity.ok(this.customerService.deleteCustomer(id));
     }
 
     @Override
     public ResponseEntity<CustomerResponse> findCustomerById(UUID id) {
-        try {
-            return ResponseEntity.ok(this.customerService.getCustomerDetails(id));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        return ResponseEntity.ok(this.customerService.getCustomerDetails(id));
     }
 
     @Override
     public ResponseEntity<List<CustomerResponse>> findCustomers(Integer offset, Integer limit) {
-        try {
-            return ResponseEntity.ok(this.customerService.getCustomers(limit, offset));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        return ResponseEntity.ok(this.customerService.getCustomers(limit, offset));
     }
 
     @Override
     public ResponseEntity<CustomerResponse> updateCustomer(UUID id, CustomerRequest customerRequest) {
-        try {
-            return ResponseEntity.ok(customerService.updateCustomer(id, customerRequest));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        return ResponseEntity.ok(customerService.updateCustomer(id, customerRequest));
     }
 }
