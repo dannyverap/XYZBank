190e802de098ead9a6225017130e9fc38b005dfa
dverapal <dannysteep.verapalomino@.nttdata.com>
2024-10-03 16:34:18 -0500

    Add findByClienteId method to account repository

--- AccountMs/AccountMs/src/main/java/com/danny/AccountMs/repository/AccountRepository.java
+++ AccountMs/AccountMs/src/main/java/com/danny/AccountMs/repository/AccountRepository.java
@@ -1,10 +1,13 @@
 package com.danny.AccountMs.repository;
 
 import com.danny.AccountMs.model.Account;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface AccountRepository extends JpaRepository<Account, UUID> {
     boolean existsByNumeroCuenta(String numeroCuenta);
+    Page<Account> findByClienteId(UUID clienteId, Pageable pageable);
 }
