dfc00a0b0332eaaff5fa27dd746b6965f9e6300f
dverapal <dannysteep.verapalomino@.nttdata.com>
2024-10-03 16:35:19 -0500

    Add clienteId parameter to getAccounts method

--- AccountMs/AccountMs/src/main/java/com/danny/AccountMs/business/AccountService.java
+++ AccountMs/AccountMs/src/main/java/com/danny/AccountMs/business/AccountService.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 
 public interface AccountService {
     AccountResponse createAccount (AccountRequest accountRequest);
-    List<AccountResponse> getAccounts(int limit, int offset);
+    List<AccountResponse> getAccounts(int limit, int offset,UUID clienteId);
     AccountResponse getAccountDetails(UUID id);
     AccountResponse updateAccount(UUID id, AccountRequest newAccountData);
     ModelApiResponse deleteAccount(UUID id);
