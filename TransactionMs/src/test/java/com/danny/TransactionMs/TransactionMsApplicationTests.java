35dbb74033d25dd72447355bec1e2dd71969ad9e
dverapal <dannysteep.verapalomino@.nttdata.com>
2024-10-06 09:06:19 -0500

    Initial commit TransactionMs

---
+++ TransactionMs/src/test/java/com/danny/TransactionMs/TransactionMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danny.TransactionMs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
