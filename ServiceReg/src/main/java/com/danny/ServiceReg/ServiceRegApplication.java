432e2ed617c20cfeff8c8abf5323b465bf209bb7
dverapal <dannysteep.verapalomino@.nttdata.com>
2024-10-06 09:38:40 -0500

    Add Todo: Solve problem with host register as "host.docker.internal:" / "NTT.." om RegistryService

--- ServiceReg/src/main/java/com/danny/ServiceReg/ServiceRegApplication.java
+++ ServiceReg/src/main/java/com/danny/ServiceReg/ServiceRegApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+//TODO: Solve problem with host register as "host.docker.internal:" / "NTT.."
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceRegApplication {
