f2916f85e204d4e56d9b3bf5110bf81e442d3f40
dverapal <dannysteep.verapalomino@.nttdata.com>
2024-10-06 11:42:29 -0500

    Refactor RestCustomerClient bean name

--- AccountMs/AccountMs/src/main/java/com/danny/AccountMs/clients/RestCustomerClient.java
+++ AccountMs/AccountMs/src/main/java/com/danny/AccountMs/clients/RestCustomerClient.java
@@ -11,16 +11,16 @@ import java.util.UUID;
 
 @Configuration
 public class RestCustomerClient {
-    String Url = "http://localhost:8081/customer/";
+    String Url = "http://localhost:8080/customer/";
 
     @Bean
-    public RestTemplate restTemplate() {
+    public RestTemplate restCustomerTemplate() {
         return new RestTemplate();
     }
 
     public void validateCustomerId(UUID id) {
         try {
-            restTemplate().getForObject(this.Url + id.toString(), CustomerResponse.class);
+            restCustomerTemplate().getForObject(this.Url + id.toString(), CustomerResponse.class);
         } catch (HttpClientErrorException e) {
             throw new BadPetitionException("El cliente no fue encontrado en el servicio de cuentas");
         } catch (Exception e) {
